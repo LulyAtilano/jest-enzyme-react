733dd6bf72e81cf3b9be0fe29972ff03f3956dc9
latilanoh@gmail.com <latilanoh@gmail.com>
2019-05-30 14:10:22 -0500

    added enzyme, enzyme-adapter-react-16 and jest-enzyme as dev dependencies

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,18 @@
-import React from 'react';
+/*import React from 'react';
 import App from './App';
 
 test('renders without error', () => {
 
+});
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { isTSAnyKeyword } from '@babel/types';
+
+test('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
